5873c82ba05fbbd87403ec8c7fa872ef1a34c878
eduardmath <fpm.poddubsk@bsu.by>
2022-09-30 23:02:10 +0300

    mod server.go

--- server.go
+++ server.go
@@ -6,23 +6,29 @@ import (
 	"strings"
 )
 
-func process(pull map[int]net.Conn, number int) {
-	var clientNo int
+func process(pull *map[string]net.Conn, c net.Conn) {
+	var friend string
 	buf := make([]byte, 1024*8)
 
 	// получаем доступ к текущему соединению
-	conn := pull[number]
+	conn := c
+
+	r_len, _ := conn.Read(buf)
+
+	var name = string(buf[:r_len])
+
+	(*pull)[name] = conn
 
 	// определим, что перед выходом из функции, мы закроем соединение
-	fmt.Println("Accept cnn:", number)
+	fmt.Println("Accept cnn:", name)
 	defer conn.Close()
 
 	for {
-		readed_len, err := pull[number].Read(buf)
+		readed_len, err := conn.Read(buf)
 		if err != nil {
 			if err.Error() == "EOF" {
-				fmt.Println("Close ", number)
-				delete(pull, number)
+				fmt.Println("Close ", name)
+				delete(*pull, name)
 				break
 			}
 			fmt.Println(err)
@@ -33,36 +39,38 @@ func process(pull map[int]net.Conn, number int) {
 		var message = string(buf[:readed_len])
 
 		// парсинг полученного сообщения
-		_, err = fmt.Sscanf(message, "%d", &clientNo) // определи номер клиента
+		_, err = fmt.Sscanf(message, "%s", &friend) // определи номер клиента
+
 		if err != nil {
 			// обработка ошибки формата
 			conn.Write([]byte("error format message\n"))
 			continue
 		}
 		pos := strings.Index(message, " ") // нашли позицию разделителя
+		// friend = message[:pos]
+		// _f, err = fmt.Sscan(message[:pos], &friend) // определи номер клиента
 
 		if pos > 0 {
 			out_message := message[pos+1:] // отчистили сообщение от номера клиента
 			// Распечатываем полученое сообщение
 
 			// if buf[0] == 0 {
-			conn = pull[clientNo]
+			conn = (*pull)[friend]
 			if conn == nil {
-				pull[number].Write([]byte("client is close"))
+				(*pull)[name].Write([]byte("client is close"))
 				continue
 			}
 
 			// }
-			out_buf := []byte(fmt.Sprintf("%d->>%s\n", clientNo, out_message))
+			out_buf := []byte(fmt.Sprintf("%d->>%s\n", friend, out_message))
 
 			// Отправить новую строку обратно клиенту
-			_, err2 := conn.Write(out_buf)
+			_, errWrite := conn.Write(out_buf)
 
 			// анализируем на ошибку
-			if err2 != nil {
-				fmt.Println("Error:", err2.Error())
-
-				break // выходим из цикла
+			if errWrite != nil {
+				fmt.Println("Error:", errWrite.Error())
+				break
 			}
 		}
 
@@ -74,7 +82,7 @@ func main() {
 	fmt.Println("Start server...")
 
 	// create a pull connect
-	pull := make(map[int]net.Conn, 1)
+	pull := make(map[string]net.Conn, 1)
 
 	// listen port
 	ln, err := net.Listen("tcp", ":8081")
@@ -86,11 +94,9 @@ func main() {
 	}
 	defer ln.Close()
 
-	fmt.Println("error before for")
-
 	// Запускаем цикл обработки соединений
-	for i := 0; ; i++ {
-		// Принимаем входящее соединение
+	for {
+		// Ждём входящее соединение
 		conn, err := ln.Accept()
 
 		if err != nil {
@@ -98,9 +104,9 @@ func main() {
 		}
 
 		// сохраняем соединение в пул
-		pull[i] = conn
+		// pull[i] = conn
 
 		// запускаем функцию process(conn)   как горутину
-		go process(pull, i)
+		go process(&pull, conn)
 	}
 }
